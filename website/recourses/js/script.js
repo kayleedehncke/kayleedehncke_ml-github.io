aaee2ed6a2313a42c95a33589e5fc794a2f65180
Kaylee Dehncke <kade9897@colorado.edu>
2025-09-17 16:08:04 -0600

    complete organization

--- website/recourses/js/script.js
+++ website/recourses/js/script.js
@@ -1,6 +1,5 @@
-//const nav = document.querySelector('.navbar');
 // Fetch the navbar HTML and insert it into the page
-fetch('navbar.html')
+fetch('../layouts/navbar.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar-placeholder').innerHTML = data;
@@ -23,25 +22,3 @@ fetch('navbar.html')
             }
         });
     })
-
-/* <script>
-        fetch('navbar.html')
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById('navbar-placeholder').innerHTML = data;
-
-                // Highlight current page link
-                let currentPage = window.location.pathname.split("/").pop().toLowerCase().split("?")[0].split("#")[0];
-                if (!currentPage) currentPage = "home.html";
-                const navLinks = document.querySelectorAll('#navbar-placeholder .nav-link');
-                navLinks.forEach(link => {
-                    if (link.getAttribute('href').toLowerCase() === currentPage) {
-                        link.classList.add('active');
-                        link.setAttribute('aria-current', 'page');
-                    } else {
-                        link.classList.remove('active');
-                        link.removeAttribute('aria-current');
-                    }
-                });
-            });
-    </script> */
